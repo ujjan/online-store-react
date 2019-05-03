4c9086f0c0a7e5b1ff29f652753f886a422593bc
Ayaz Ali <ayaz.ujjan@gmail.com>
2019-05-03 02:26:58 +0200

    Routs has been added to all diffrent pages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
 import logo from "./logo.svg";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -12,14 +13,13 @@ import ProductList from "./components/ProductList";
 function App() {
   return (
     <React.Fragment>
-      <Cart />
-      <Default />
-      <Details />
       <Navbar />
-      <Product />
-      <ProductList />
-
-      <h3>Hello from App</h3>
+      <Switch>
+        <Route exact path="/" component={ProductList} />
+        <Route path="/details" component={Details} />
+        <Route path="/cart" component={Cart} />
+        <Route component={Default} />
+      </Switch>
     </React.Fragment>
   );
 }
